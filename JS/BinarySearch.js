1c5a40943f2ed089ec48fbb273679a0a8127a0f1
Andreas Brandhøj <andreasbrandhoej@hotmail.com>
2020-03-18 09:56:30 +0100

    Binary search now uses generic compare function
    But the quick return functionality is now broken. Because we dont know what order the array is sorted in.

--- JS/BinarySearch.js
+++ JS/BinarySearch.js
@@ -12,28 +12,33 @@
  *        A sorted array of numbers.
  * @param {Number} key
  *        The key element value to search for
+ * @param {Function} cmp
+ *        the compare function used on element element and key. returns are -1, 0, 1
  * @return {Number}
  *         the index of the key. If not found then -1.
  */
-function binarySearch(arr, key) {
+function binarySearch(arr, key, cmp) {
     function search(left = 0, right = arr.length - 1) {
         /*
-         * Check if the the search range is still correct. 
+         * Check if the the search range is still correct.
+         * Does not work with current implementation
          * If not then the item is not in the array
          */
-        if(arr[left] > key || arr[right] < key || left > right) {
-            return -1;
-        }
+        // if(arr[left] > key || arr[right] < key || left > right) {
+        //     return -1;
+        // }
 
         let mid = Math.floor((left + right) / 2.0);
-
-        if(arr[mid] == key) {
+        
+        let cmpRes = cmp(arr[mid], key);
+        
+        if(cmpRes === 0) {
             return mid;
         }
-        else if(arr[mid] > key) {
+        else if(cmpRes === -1) {
             return search(left, mid - 1);
         }
-        else if(arr[mid] < key) {
+        else if(cmpRes === 1) {
             return search(mid + 1, right);
         }
     }
